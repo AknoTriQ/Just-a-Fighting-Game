9abeb5dfd66be5865fbf0202d1f32236e415c132
AknoTriQ <57116758+AknoTriQ@users.noreply.github.com>
2019-10-31 16:28:42 +0100

    Ez Most Jó Lesz!
    de tényleg

--- Sceens/Camera2D.gd
+++ Sceens/Camera2D.gd
@@ -1,6 +1,4 @@
 extends Camera2D
 
-
 func _ready():
 	make_current()
-
--- Sceens/Player 1.tscn.gd
+++ Sceens/Player 1.tscn.gd
@@ -1,44 +1,43 @@
 extends KinematicBody2D
 
-const UP = Vector2(0, -1)
-const GRAVITY = 14
+const GRAVITY = 10
 const SPEED = 80
-const JUMP_HEIGHT = 200
+const JUMP_HEIGHT = 220
+const UP = Vector2(0, -1)
+
 var motion = Vector2()
 
 func _physics_process(delta):
+	
 	motion.y += GRAVITY
-	if motion.x == 0:
-		if is_on_floor():
-			$AnimatedSprite.play("Idle")
 	
 	if Input.is_action_pressed("RIGHT"):
-		motion.x = +SPEED
+		motion.x = SPEED
 		if is_on_floor():
 			$AnimatedSprite.play("Run")
 			$AnimatedSprite.flip_h = false
 	else:
 		motion.x = 0
-		
-		
+	
+	
 	if Input.is_action_pressed("LEFT"):
 		motion.x = -SPEED
 		if is_on_floor():
 			$AnimatedSprite.play("Run")
 			$AnimatedSprite.flip_h = true
 	
-		
+	
+	
 	if Input.is_action_pressed("JUMP"):
 		if is_on_floor():
 			motion.y = -JUMP_HEIGHT
+			
+	if motion.y != 0:
+		if not is_on_floor():
 			$AnimatedSprite.play("Jump")
-	
-	
-	
-	
-	
-	
-	
-	
-	
+			
+	if motion.x == 0:
+		if is_on_floor():
+			$AnimatedSprite.play("Idle")
+
 	motion = move_and_slide(motion, UP)
